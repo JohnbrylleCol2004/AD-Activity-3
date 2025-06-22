af1e0c9c1a96053b420d9ee15aadc752c7818658
JohnBryllCol2004 <202310280@fit.edu.ph>
2025-06-22 22:26:53 +0800

    feat: added some features in index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include_once "./handlers/postgreChecker.handler.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Checker</title>
+</head>
+
+<body>
+    <h1>Database Connection Status:</h1>
+</body>
+
+</html>
